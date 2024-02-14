55407a1af10a5dc02525f7e05c9f3f24600308ac
saifulislam888911 <saiful.islam888911@gmail.com>
2024-02-15 01:49:15 +0600

    Commit No.1 - Write,SqlQuerry,Crud-commit 1

---
+++ AdoDotNetProject/AdoDotNetProject/AdonetUtility.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace AdoDotNetProject
+{
+    public class AdonetUtility
+    {
+        private readonly string _connectionString;
+
+        public AdonetUtility(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void ExecuteSql(string sql)
+        {
+            /* [Note: * If the clase impliments "Idisposal" interface ,
+                        then keyword "using" will dispose automatically.]
+            */
+
+            using SqlConnection connection = new SqlConnection(_connectionString);      
+            using SqlCommand command = new SqlCommand(sql, connection);     /*[Note: here sql=sql query, connection=connection object.]*/
+
+
+            if (connection.State != System.Data.ConnectionState.Open) 
+                connection.Open();
+
+
+            int effection = command.ExecuteNonQuery();      /*[Note: It will count - How many rows are affected.]*/
+            Console.WriteLine("Row is effected : " + " " + effection);
+
+            /* [Note: * SqlConnection, SqlCommand These classes's methods are heavy & occupied the ram ,
+                        That is why after every operation it must be destroyed.
+                      * if "using" keyword is used , dont need to write below codes.]
+            */
+
+            //connection.Close();
+            //command.Dispose();
+            //connection.Dispose();
+        }
+    }
+}
---
+++ AdoDotNetProject/AdoDotNetProject/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdoDotNetProject;
+
+
+
+string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;Database=Prac_CSharpB15;User Id=Practice_Project;Password=123456;Trust Server Certificate=True;";
+
+
+string insertSql = "INSERT INTO Students(Name, Address, CGPA) VALUES('Mr. E', 'Hamburg, Germany', 4.00)";
+string updateSql = "UPDATE Students SET Name='Mr. CCC', Address='India' WHERE Id=3";
+string deleteSql = "DELETE FROM Students WHERE Id=3";
+
+
+AdonetUtility adonetUtility = new AdonetUtility(connectionString);
+adonetUtility.ExecuteSql(insertSql);
+Console.WriteLine("Done");
