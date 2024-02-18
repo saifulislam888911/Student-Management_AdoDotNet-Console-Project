d9ff0966ddcc609cdae13c33aa10c28d6540c484
SAIFUL ISLAM <saiful.islam888911@gmail.com>
2024-02-18 20:05:38 +0600

    Commit No.4 - Code Organizing-commit 1
    # Organize the Tutorial Code

--- AdoDotNetProject/AdoDotNetProject/Program.cs
+++ AdoDotNetProject/AdoDotNetProject/Program.cs
@@ -2,16 +2,26 @@
 
 
 
-string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;Database=Prac_CSharpB15;User Id=Practice_Project;Password=123456;Trust Server Certificate=True;";
+Console.WriteLine("Welcome To \"Student Management System\".");
 
-string[] parts = Console.ReadLine().Split(",");
-int id = int.Parse(Console.ReadLine());
+//string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;Database=Prac_CSharpB15;User Id=Practice_Project;Password=123456;Trust Server Certificate=True;";
+string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;" +
+                          "Database=Prac_CSharpB15;" +
+                          "User Id=Practice_Project;" +
+                          "Password=123456;" +
+                          "Trust Server Certificate=True;";
 
 string insertSql = "INSERT INTO Students(Name, Address, CGPA) VALUES(@name, @address, @cgpa)";
-string updateSql = "UPDATE Students SET Name='Mr. CCC', Address='India' WHERE Id=@id";
+string updateSql = "UPDATE Students SET Name=@name, Address=@address, CGPA=@cgpa WHERE Id=@id";
 string deleteSql = "DELETE FROM Students WHERE Id=@id";
 string selectSql = "SELECT * FROM Students WHERE Id=@id";
 
+
+Console.WriteLine("Insert Operation:");
+string[] parts = Console.ReadLine().Split(",");
+
+Console.WriteLine("Select Operation:");
+int id = int.Parse(Console.ReadLine());
 
 Dictionary<string, object> parameters1 = new Dictionary<string, object>();
 parameters1.Add("name", parts[0]);
@@ -21,9 +31,21 @@ parameters1.Add("cgpa", double.Parse(parts[2]));
 Dictionary<string, object> parameters2 = new Dictionary<string, object>();
 parameters2.Add("id", id);
 
+
+
+/* .................... Creating DB-Connection Object .................... */
+
 AdonetUtility adonetUtility = new AdonetUtility(connectionString);
 
+
+/* .................... Calling DB-Connection Object's SQL Methods .................... */
+
+/* ..... Insert Operation .....*/
+
 adonetUtility.ExecuteSql(insertSql, parameters1);
+
+
+/* ..... Select Operation .....*/
 
 //var result = adonetUtility.GetData(selectSql);        
 IList<Dictionary<string, object>> result = adonetUtility.GetData(selectSql, parameters2);       
