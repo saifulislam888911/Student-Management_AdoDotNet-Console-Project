579ea21a58b96ff9a93b90928532e6796f4aa30e
SAIFUL ISLAM <saiful.islam888911@gmail.com>
2024-02-19 14:12:56 +0600

    Version : 2 - (Mine) - commit 1
    # Tutorial Code : is Modified , is added Some Features , Crud Operation is Updated.

--- AdoDotNetProject/AdoDotNetProject/Program.cs
+++ AdoDotNetProject/AdoDotNetProject/Program.cs
@@ -1,8 +1,19 @@
 ﻿using AdoDotNetProject;
+using System.Reflection.Metadata;
 
 
 
 Console.WriteLine("Welcome To \"Student Management System\"." + "\n");
+Console.WriteLine("Operation:" + "\n" +
+                  "1.Insert" + "\n" +
+                  "2.Update" + "\n" +
+                  "3.Delete" + "\n" +
+                  "4.Select" + "\n" + 
+                  "5.Display Full Table" + "\n" +
+                  "6.Close" + "\n" + "\n" +
+                  "Select The Querry:(Write The Serial Number)");
+
+string operation = Console.ReadLine();
 
 //string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;Database=Prac_CSharpB15;User Id=Practice_Project;Password=123456;Trust Server Certificate=True;";
 string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;" +
@@ -14,25 +25,8 @@ string connectionString = "Server=HP-LAPTOP\\SQLEXPRESS;" +
 string insertSql = "INSERT INTO Students(Name, Address, CGPA) VALUES(@name, @address, @cgpa)";
 string updateSql = "UPDATE Students SET Name=@name, Address=@address, CGPA=@cgpa WHERE Id=@id";
 string deleteSql = "DELETE FROM Students WHERE Id=@id";
-string selectSql = "SELECT * FROM Students WHERE Id=@id";
-
-
-
-Console.WriteLine("\n" + "Insert Operation:");
-Console.WriteLine("Format:Name,Address,CGPA");
-string[] parts = Console.ReadLine().Split(",");
-
-Console.WriteLine("\n" + "Select Operation:");
-Console.WriteLine("Format:(Select By ID)");
-int id = int.Parse(Console.ReadLine());
-
-Dictionary<string, object> parameters1 = new Dictionary<string, object>();
-parameters1.Add("name", parts[0]);
-parameters1.Add("address", parts[1]);
-parameters1.Add("cgpa", double.Parse(parts[2]));
-
-Dictionary<string, object> parameters2 = new Dictionary<string, object>();
-parameters2.Add("id", id);
+string selectByIdSql = "SELECT * FROM Students WHERE Id=@id";
+string selectFullTableSql = "SELECT * FROM Students";
 
 
 
@@ -44,27 +38,96 @@ AdonetUtility adonetUtility = new AdonetUtility(connectionString);
 
 /* .................... Calling DB-Connection Object's SQL Methods .................... */
 
-/* ..... Insert Operation .....*/
-
-adonetUtility.ExecuteSql(insertSql, parameters1);
-
-
-
-/* ..... Select Operation .....*/
-
-IList<Dictionary<string, object>> result = adonetUtility.GetData(selectSql, parameters2);
-//var result = adonetUtility.GetData(selectSql, parameters2); 
-
-foreach (var row in result)
+if (operation == "1" || operation == "Insert")
 {
-    foreach(var col in row)
+    Console.WriteLine("\n" + "Insert Operation:");
+    Console.WriteLine("Format:Name,Address,CGPA");
+    string[] parts = Console.ReadLine().Split(",");
+
+    Dictionary<string, object> parameters1 = new Dictionary<string, object>();
+    parameters1.Add("name", parts[0]);
+    parameters1.Add("address", parts[1]);
+    parameters1.Add("cgpa", double.Parse(parts[2]));
+
+    adonetUtility.ExecuteSql(insertSql, parameters1);
+}
+if (operation == "2" || operation == "Update")
+{
+    Console.WriteLine("\n" + "Update Operation:");
+    Console.WriteLine("Input ID :");
+    int id = int.Parse(Console.ReadLine());
+    Console.WriteLine("Format:Name,Address,CGPA");
+    string[] parts = Console.ReadLine().Split(",");
+
+    Dictionary<string, object> parameters2 = new Dictionary<string, object>();
+    parameters2.Add("id", id);
+    parameters2.Add("name", parts[0]);
+    parameters2.Add("address", parts[1]);
+    parameters2.Add("cgpa", double.Parse(parts[2]));
+
+    adonetUtility.ExecuteSql(updateSql, parameters2);
+}
+if (operation == "3" || operation == "Delete")
+{
+    Console.WriteLine("\n" + "Delete Operation:");
+    Console.WriteLine("Input ID :");
+    int id = int.Parse(Console.ReadLine());
+
+    Dictionary<string, object> parameters3 = new Dictionary<string, object>();
+    parameters3.Add("id", id);
+
+    adonetUtility.ExecuteSql(deleteSql, parameters3);
+}
+if (operation == "4" || operation == "Select")
+{
+    Console.WriteLine("\n" + "Select Operation:");
+    Console.WriteLine("Format:(Select By ID)");
+    int id = int.Parse(Console.ReadLine());
+
+    Dictionary<string, object> parameters4 = new Dictionary<string, object>();
+    parameters4.Add("id", id);
+
+    IList<Dictionary<string, object>> result = adonetUtility.GetData(selectByIdSql, parameters4);
+    //var result = adonetUtility.GetData(selectSql, parameters4); 
+
+    foreach (var row in result)
     {
-        Console.Write(col.Value);
+        foreach (var col in row)
+        {
+            Console.Write(col.Value);
 
-        Console.Write(" , ");
+            Console.Write(" , ");
+        }
+
+        Console.WriteLine();
     }
+}
+if (operation == "5" || operation == "Display Full Table")
+{
+    Console.WriteLine("\n" + "Display Full Table:(Select Entire Table Operation)" + "\n" +
+                      "Format: Id , Name , Address , CGPA" + "\n" +
+                      "Table : Students" + "\n");
 
-    Console.WriteLine();
+    Dictionary<string, object> parameters5 = new Dictionary<string, object>();
+
+    IList<Dictionary<string, object>> result = adonetUtility.GetData(selectFullTableSql, parameters5);
+    //var result = adonetUtility.GetData(selectFullTableSql, parameters5); 
+
+    foreach (var row in result)
+    {
+        foreach (var col in row)
+        {
+            Console.Write(col.Value);
+
+            Console.Write(" , ");
+        }
+
+        Console.WriteLine();
+    }
+}
+if (operation == "6" || operation == "Close")
+{
+
 }
 
 
